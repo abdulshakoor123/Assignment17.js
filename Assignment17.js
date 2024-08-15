7b7e061649380f2a7b7dd7c844f47d7e9f41610d
abdulshakoor123 <hangu3456789@gmail.com>
2024-08-15 11:54:39 +0500

    added important css files

---
+++ Assignment17.js
@@ -0,0 +1,14 @@
+// What is function?
+// function is a block of javascript robotically that does the same things. again and again
+// why function ?
+// It saves you repetative coding and makes your code easier to understand
+// example scenario 
+// on you website suppose there are several pages where you want to display alert that tells users the current time. 
+// without creating function 
+// const now = new Date();
+// const theHr = now.getHours();
+// const theMin = now.getMinutes();
+// alert("time:"+ theHr +":" + theMin);
+
+// usig functions
+Function
